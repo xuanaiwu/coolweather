4bc48573e73811b30d3c127d2ffd6631b661f131
xuanaiwu <xuanaiwu@gmail.com>
2016-10-09 23:18:41 +0800

    20161009

---
+++ app/src/main/java/app/coolweather/com/model/City.java
@@ -0,0 +1,7 @@
+package app.coolweather.com.model;
+
+/**
+ * Created by Administrator on 2016/10/9.
+ */
+public class City {
+}
---
+++ app/src/main/java/app/coolweather/com/model/Province.java
@@ -0,0 +1,38 @@
+package app.coolweather.com.model;
+
+/**
+ * Created by Administrator on 2016/10/9.
+ */
+public class Province {
+
+    private  int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void  setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+
+
+    public  String getProvinceCode(){
+        return provinceCode;
+
+    }
+
+    public void  setProvinceCode(){
+        this.provinceCode=provinceCode;
+    }
+
+
+}
